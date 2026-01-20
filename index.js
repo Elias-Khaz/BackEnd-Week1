1d72b15b1bb4e32095cd5dd815f170adf22e9e6d
Elias-Khaz <elias.khazroun5@gmail.com>
2026-01-20 16:05:31 +0100

    lesson 1 finished

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ console.log(`User url: ${userUrl}`)
 
 
 // Lesson 2
+const firstName = "Elias"
+const birthYear = 2005
+const likesCoding = true
 
+console.log(`Hi, my name is ${firstName}, I was born ${birthYear}, and it is ${likesCoding} that I like coding.`);
