9099ddf36edb8d845ce5cdbeb83a441f53b94a14
Elias-Khaz <elias.khazroun5@gmail.com>
2026-01-20 16:06:32 +0100

    lesson 1 completed

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 // Lesson 1
 
-const baseUrl = "https://svt.se"
-const userId = "12345"
-const userUrl = baseUrl + "/user/" + userId
-
-console.log(`User url: ${userUrl}`)
-
-
-
-
-// Lesson 2
 const firstName = "Elias"
 const birthYear = 2005
 const likesCoding = true
